f6a97f79537e75e9f6461be4dd94b9d155aedb4d
Victor Lupascu <sinajeen@gmail.com>
2022-03-11 15:45:27 -0600

    add Post model and populate the db with seeds

--- seeds.py
+++ seeds.py
@@ -1,4 +1,4 @@
-from app.models import User
+from app.models import User, Post
 from app.db import Session, Base, engine
 # drop and rebuild tables
 Base.metadata.drop_all(engine)
@@ -14,4 +14,16 @@ db.add_all([
   User(username='djiri4', email='gmidgley4@weather.com', password='password123')
 ])
 db.commit()
+
+# insert posts
+db.add_all([
+  Post(title='Donec posuere metus vitae ipsum', post_url='https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png', user_id=1),
+  Post(title='Morbi non quam nec dui luctus rutrum', post_url='https://nasa.gov/donec.json', user_id=1),
+  Post(title='Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue', post_url='https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx', user_id=2),
+  Post(title='Nunc purus', post_url='http://desdev.cn/enim/blandit/mi.jpg', user_id=3),
+  Post(title='Pellentesque eget nunc', post_url='http://google.ca/nam/nulla/integer.aspx', user_id=4)
+])
+
+db.commit()
+
 db.close()
