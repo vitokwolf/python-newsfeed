3894e3f27aede580fa7f2caca947c33e4a7e3015
Victor Lupascu <sinajeen@gmail.com>
2022-03-12 15:43:29 -0600

    add filters/helpers

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from .routes import home, dashboard
 from app.db import init_db
+from app.utils import filters
 
 def create_app(test_config=None):
     #set up app config
@@ -9,6 +10,11 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='super_secret_key'
     )
+
+    #register filters/helpers to jinja
+    app.jinja_env.filters['format_url'] = filters.format_url
+    app.jinja_env.filters['format_date'] = filters.format_date
+    app.jinja_env.filters['format_plural'] = filters.format_plural
 
     @app.route('/hello')
     def hello():
