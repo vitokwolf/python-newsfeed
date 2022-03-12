b1f70e2f7453918e0d68270cc0de06e2ec29ab73
Victor Lupascu <sinajeen@gmail.com>
2022-03-12 15:02:02 -0600

    setup db connection

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from flask import g
 
 load_dotenv()
 
@@ -10,3 +11,13 @@ load_dotenv()
 engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+def init_db():
+    Base.metadata.create_all(engine)
+
+def get_db():
+    if 'db' not in g:
+        #store db connection in app context
+        g.db = Session()
+        
+    return Session()
